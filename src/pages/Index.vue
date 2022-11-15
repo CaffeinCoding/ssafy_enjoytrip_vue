3d37fdd23c3659d48f116402a1d0f1340ce154ad
too519 <jintea519@gmail.com>
2022-11-15 17:52:57 +0900

    FIX carouselSection Size

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <div class="page-header clear-filter">
-      <carousel-section></carousel-section>
+    <carousel-section></carousel-section>
 
-      <div class="container">
+    <!-- <div class="container">
         <div class="content-center brand">
           <img class="n-logo" src="img/now-logo.png" alt="" />
           <h1 class="h1-seo">Now UI Kit.</h1>
           <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
         </div>
-      </div>
-    </div>
+      </div> -->
+
     <basic-elements></basic-elements>
     <navigation></navigation>
     <tabs-section></tabs-section>
--- src/pages/components/CarouselSection.vue
+++ src/pages/components/CarouselSection.vue
@@ -2,44 +2,29 @@
   <div class="section" id="carousel">
     <div class="row justify-content-center">
       <div class="col">
-        <el-carousel trigger="hover" height="900px">
+        <el-carousel trigger="click" :height="`${carouselHeight}px`">
           <el-carousel-item
-            style="
-              background-image: url('img/bg1.jpg');
-              background-repeat: no-repeat;
-              background-size: cover;
-              background-position: center;
-            "
+            id="carousel-item"
+            style="background-image: url('img/bg1.jpg')"
           >
-            <!-- <img class="d-block" src="img/bg1.jpg" alt="First slide" /> -->
-            <div class="carousel-caption d-none d-md-block">
+            <div class="carousel-item-caption d-none d-md-block">
               <h5>Nature, United States</h5>
             </div>
           </el-carousel-item>
           <el-carousel-item
-            style="
-              background-image: url('img/bg3.jpg');
-              background-repeat: no-repeat;
-              background-size: cover;
-              background-position: center;
-            "
+            id="carousel-item"
+            style="background-image: url('img/bg3.jpg')"
           >
-            <!-- <img class="d-block" src="img/bg3.jpg" alt="Second slide" /> -->
-            <div class="carousel-caption d-none d-md-block">
+            <div class="carousel-item-caption d-none d-md-block">
               <h5>Somewhere Beyond, United States</h5>
             </div>
           </el-carousel-item>
           <el-carousel-item
-            style="
-              background-image: url('img/bg4.jpg');
-              background-repeat: no-repeat;
-              background-size: cover;
-              background-position: center;
-            "
+            id="carousel-item"
+            style="background-image: url('img/bg4.jpg')"
           >
-            <!-- <img class="d-block" src="img/bg4.jpg" alt="Third slide" /> -->
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Yellowstone National Park, United States</h5>
+            <div class="carousel-item-caption d-none d-md-block">
+              <h2>Yellowstone National Park, United States</h2>
             </div>
           </el-carousel-item>
         </el-carousel>
@@ -55,14 +40,79 @@ export default {
     [Carousel.name]: Carousel,
     [CarouselItem.name]: CarouselItem,
   },
+  data() {
+    return {
+      carouselHeight: window.innerHeight,
+      observer: null,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  methods: {
+    onResize() {
+      this.carouselHeight = window.innerWidth / 2;
+    },
+  },
+  created() {
+    this.onResize();
+    // this.observer = new IntersectionObserver((e) => {
+    //   e.forEach((item) => {
+    //     if (item.isIntersecting) {
+    //       item.target.style.opacity = 1;
+    //     } else {
+    //       item.target.style.opacity = 0;
+    //     }
+    //   });
+    // });
+    // const caption = document.querySelectorAll(".carousel-item-caption");
+    // caption.forEach((cap) => {
+    //   this.observer.observe(cap);
+    // });
+  },
 };
 </script>
 <style scoped>
 #carousel {
   margin-top: -80px;
 }
-img {
+
+#carousel-item {
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: top;
+}
+#carousel-item::after {
+  display: block;
+  content: "";
+  position: absolute;
   width: 100%;
   height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.carousel-item-caption {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  color: white;
+  text-align: center;
+  opacity: 0;
+  transition: all 2.5s;
+}
+
+.is-active > .carousel-item-caption {
+  opacity: 1;
 }
 </style>
