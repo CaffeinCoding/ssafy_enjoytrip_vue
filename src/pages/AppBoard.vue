cb6c07f166786c46bdfacba1c88eddb09020467e
too519 <jintea519@gmail.com>
2022-11-16 09:59:42 +0900

    ADD header footer AppBoard

--- src/pages/AppBoard.vue
+++ src/pages/AppBoard.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h2>Vue를 이용한 게시판</h2>
-    <router-view></router-view>
+  <div class="page-header">
+    <div class="container">
+      <h2>Vue를 이용한 게시판</h2>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
---
+++ src/pages/AppTour.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: "AppTour",
+};
+</script>
+
+<style></style>
